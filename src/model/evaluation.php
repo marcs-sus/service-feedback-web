87a32bab7cc45b7932704e23e65881b0a707793e
Marcos <lilianemarcos2018@gmail.com>
2025-12-01 16:10:09 -0300

    Add Evaluation and Feedback models with finder methods

---
+++ src/model/evaluation.php
@@ -0,0 +1,156 @@
+<?php
+require_once '../query.php';
+require_once 'sector.php';
+require_once 'question.php';
+require_once 'device.php';
+
+class Evaluation
+{
+    private int $id;
+    private Sector $sector;
+    private Question $question;
+    private Device $device;
+    private int $score;
+    private DateTime $created_at;
+
+    public function __construct(int $id, Sector $sector, Question $question, Device $device, int $score, DateTime $created_at)
+    {
+        $this->id = $id;
+        $this->sector = $sector;
+        $this->question = $question;
+        $this->device = $device;
+        $this->score = $score;
+        $this->created_at = $created_at;
+    }
+
+    public static function find_by_id(int $id): ?Evaluation
+    {
+        $evaluation_query = new Query();
+        $evaluations = $evaluation_query->select(TABLE_EVALUATIONS, ['*'], [COLUMNS_EVALUATIONS['id'] => $id]);
+
+        $evaluation = $evaluations[0] ?? null;
+        if ($evaluation) {
+            return new Evaluation(
+                $evaluation[COLUMNS_EVALUATIONS['id']],
+                Sector::find_by_id($evaluation[COLUMNS_EVALUATIONS['sector_id']]),
+                Question::find_by_id($evaluation[COLUMNS_EVALUATIONS['question_id']]),
+                Device::find_by_id($evaluation[COLUMNS_EVALUATIONS['device_id']]),
+                $evaluation[COLUMNS_EVALUATIONS['score']],
+                $evaluation[COLUMNS_EVALUATIONS['created_at']]
+            );
+        }
+
+        return null;
+    }
+
+    public static function find_all(): array
+    {
+        $evaluation_query = new Query();
+        $evaluations = $evaluation_query->select(TABLE_EVALUATIONS);
+
+        foreach ($evaluations as $key => $value) {
+            $evaluations[$key] = new Evaluation(
+                $value[COLUMNS_EVALUATIONS['id']],
+                Sector::find_by_id($value[COLUMNS_EVALUATIONS['sector_id']]),
+                Question::find_by_id($value[COLUMNS_EVALUATIONS['question_id']]),
+                Device::find_by_id($value[COLUMNS_EVALUATIONS['device_id']]),
+                $value[COLUMNS_EVALUATIONS['score']],
+                $value[COLUMNS_EVALUATIONS['created_at']]
+            );
+        }
+
+        return $evaluations;
+    }
+
+    public static function find_all_by_sector(int $sector_id): array
+    {
+        $evaluation_query = new Query();
+        $evaluations_result = $evaluation_query->select(
+            TABLE_EVALUATIONS,
+            ['*'],
+            [COLUMNS_EVALUATIONS['sector_id'] => $sector_id]
+        );
+
+        $evaluations = [];
+        foreach ($evaluations_result as $evaluation) {
+            $evaluations[] = new Evaluation(
+                $evaluation[COLUMNS_EVALUATIONS['id']],
+                Sector::find_by_id($evaluation[COLUMNS_EVALUATIONS['sector_id']]),
+                Question::find_by_id($evaluation[COLUMNS_EVALUATIONS['question_id']]),
+                Device::find_by_id($evaluation[COLUMNS_EVALUATIONS['device_id']]),
+                $evaluation[COLUMNS_EVALUATIONS['score']],
+                $evaluation[COLUMNS_EVALUATIONS['created_at']]
+            );
+        }
+
+        return $evaluations;
+    }
+
+    public static function calc_average_score_by_question(int $question_id): float
+    {
+        $evaluation_query = new Query();
+        $evaluation_result = $evaluation_query->select(
+            TABLE_EVALUATIONS,
+            [COLUMNS_EVALUATIONS['score']],
+            [COLUMNS_EVALUATIONS['question_id'] => $question_id]
+        );
+
+        $scores = [];
+        foreach ($evaluation_result as $key => $value) {
+            $scores[$key] = $value[COLUMNS_EVALUATIONS['score']];
+        }
+
+        $average = array_sum($scores) / count($scores);
+
+        return $average;
+    }
+
+    public static function calc_average_score_by_sector(int $sector_id): float
+    {
+        $evaluation_query = new Query();
+        $evaluation_result = $evaluation_query->select(
+            TABLE_EVALUATIONS,
+            [COLUMNS_EVALUATIONS['score']],
+            [COLUMNS_EVALUATIONS['sector_id'] => $sector_id]
+        );
+
+        $scores = [];
+        foreach ($evaluation_result as $key => $value) {
+            $scores[$key] = $value[COLUMNS_EVALUATIONS['score']];
+        }
+
+        $average = array_sum($scores) / count($scores);
+
+        return $average;
+    }
+
+    public function get_id(): int
+    {
+        return $this->id;
+    }
+
+    public function get_sector(): Sector
+    {
+        return $this->sector;
+    }
+
+    public function get_question(): Question
+    {
+        return $this->question;
+    }
+
+    public function get_device(): Device
+    {
+        return $this->device;
+    }
+
+    public function get_score(): int
+    {
+        return $this->score;
+    }
+
+    public function get_created_at(): DateTime
+    {
+        return $this->created_at;
+    }
+}
---
+++ src/model/feedback.php
@@ -0,0 +1,104 @@
+<?php
+
+
+class Feedback
+{
+    private int $id;
+    private Sector $sector;
+    private Device $device;
+    private string $feedback_text;
+    private DateTime $created_at;
+
+    public function __construct(int $id, Sector $sector, Device $device, string $feedback_text, DateTime $created_at)
+    {
+        $this->id = $id;
+        $this->sector = $sector;
+        $this->device = $device;
+        $this->feedback_text = $feedback_text;
+        $this->created_at = $created_at;
+    }
+
+    public static function find_by_id(int $id): ?Feedback
+    {
+        $feedback_query = new Query();
+        $feedbacks = $feedback_query->select(TABLE_FEEDBACK, ['*'], [COLUMNS_FEEDBACK['id'] => $id]);
+
+        $feedbacks = $feedbacks[0] ?? null;
+        if ($feedbacks) {
+            return new Feedback(
+                $feedbacks[COLUMNS_FEEDBACK['id']],
+                Sector::find_by_id($feedbacks[COLUMNS_FEEDBACK['sector_id']]),
+                Device::find_by_id($feedbacks[COLUMNS_FEEDBACK['device_id']]),
+                $feedbacks[COLUMNS_FEEDBACK['text']],
+                $feedbacks[COLUMNS_FEEDBACK['created_at']]
+            );
+        }
+
+        return null;
+    }
+
+    public static function find_all(): array
+    {
+        $feedback_query = new Query();
+        $feedbacks = $feedback_query->select(TABLE_FEEDBACK);
+
+        foreach ($feedbacks as $key => $value) {
+            $feedbacks[$key] = new Feedback(
+                $value[COLUMNS_FEEDBACK['id']],
+                Sector::find_by_id($value[COLUMNS_FEEDBACK['sector_id']]),
+                Device::find_by_id($value[COLUMNS_FEEDBACK['device_id']]),
+                $value[COLUMNS_FEEDBACK['text']],
+                $value[COLUMNS_FEEDBACK['created_at']]
+            );
+        }
+
+        return $feedbacks;
+    }
+
+    public static function find_all_by_sector(int $sector_id): array
+    {
+        $feedback_query = new Query();
+        $feedbacks = $feedback_query->select(
+            TABLE_FEEDBACK,
+            ['*'],
+            [COLUMNS_FEEDBACK['sector_id'] => $sector_id]
+        );
+
+        foreach ($feedbacks as $feedback) {
+            $feedbacks[] = new Feedback(
+                $feedback[COLUMNS_FEEDBACK['id']],
+                Sector::find_by_id($feedback[COLUMNS_FEEDBACK['sector_id']]),
+                Device::find_by_id($feedback[COLUMNS_FEEDBACK['device_id']]),
+                $feedback[COLUMNS_FEEDBACK['text']],
+                $feedback[COLUMNS_FEEDBACK['created_at']]
+            );
+        }
+
+        return $feedbacks;
+    }
+
+    public function get_id(): int
+    {
+        return $this->id;
+    }
+
+    public function get_sector(): Sector
+    {
+        return $this->sector;
+    }
+
+    public function get_device(): Device
+    {
+        return $this->device;
+    }
+
+    public function get_feedback_text(): string
+    {
+        return $this->feedback_text;
+    }
+
+    public function get_created_at(): DateTime
+    {
+        return $this->created_at;
+    }
+}
